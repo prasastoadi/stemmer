a087184cfa753f44afe65f84402eb213ca5ddebc
Prasasto Adi <prasastoadi@gmail.com>
2017-09-28 23:20:05 +0700

    Remove more than one space

--- tokenizer.py
+++ tokenizer.py
@@ -9,6 +9,7 @@ Itu karena kami ingin kelak istri dan anak kami bahagia."""
 def tokenize(text):
     text = text.lower()
     text = re.sub(r'[^a-z0-9 -]', ' ', text, flags = re.IGNORECASE|re.MULTILINE)
+    text = re.sub(r'( +)', ' ', text, flags = re.IGNORECASE|re.MULTILINE)
     tokens = text.split(" ")
     return tokens
 
